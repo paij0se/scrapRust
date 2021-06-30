967890ce651f10631d96738b98c34940e7a1bde9
pai <69026987+ELPanaJose@users.noreply.github.com>
2021-06-30 17:35:23 -0500

    error handler

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,30 @@
-use rand::seq::SliceRandom;
 use regex::Regex;
 use std::process::{Command, Stdio};
 use std::thread;
 
+const URL: &str = "https://paijose.tk"; // Here you put the url
+
 fn threads() {
-    let url = "https://github.com/ELPanaJose"; // Here you put the url
-    let samples = vec![
-        "https://github.com/ELPanaJose/",
-        "https://boards.4chan.org/s4s/",
-        "https://www.youtube.com",
-        "https://paijose.tk",
-    ];
-    let sample: Vec<_> = samples
-        .choose_multiple(&mut rand::thread_rng(), 1)
-        .collect();
-    println!("{:?}", sample); // just pick a random string from the vector
-    const NTHREADS: u32 = 2; // number of threads
+    const NTHREADS: u32 = 1; // number of threads
     let mut children = vec![];
 
-    for i in 0..NTHREADS {
+    for _i in 0..NTHREADS {
         children.push(thread::spawn(move || {
-            println!("this is thread number {}", i);
             let ou = Command::new("curl")
-                .arg(url)
+                .arg(URL)
                 .stdout(Stdio::piped())
                 .output()
                 .expect("Failed to execute command");
 
-            //let re = Regex::new(r"(https?://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?)",).unwrap(); // match all the https and http links
-            let re = Regex::new(r"(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)").unwrap(); // match all the .png, .jpg, .gif https and http links
+            let re = Regex::new(r"(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|jpeg|png)").unwrap();
             let output = String::from_utf8(ou.stdout).unwrap();
-            let formated = re.find_iter(&output);
-            for i in formated {
-                println!("{}", i.as_str())
+            let formated = re.find_iter(&output).collect::<Vec<_>>();
+            if formated.is_empty() {
+                println!("No matches from: {}", URL);
+            } else {
+                for i in formated {
+                    println!("{}", i.as_str())
+                }
             }
         }));
     }
@@ -43,6 +35,6 @@ fn threads() {
 }
 
 fn main() {
+    println!("scraping: {}", URL);
     threads();
-    println!("lmao");
 }
